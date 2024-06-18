fb898199c320a1ee3dc2cd7673a27308d3ca284a
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-06-18 20:41:52 +0200

    Update CoreModule to provide AuthService and AuthInterceptor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
-
+import { AuthService } from './services/auth.service';
+import { authGuard } from './guards/auth.guard';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  ],
   imports: [
     CommonModule
+  ],
+  providers: [
+    AuthService,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
+  exports: [
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error('CoreModule is already loaded. Import it in the AppModule only');
+    }
+  }
+}
