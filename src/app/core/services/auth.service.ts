e3fc4224605a6b33f6cfb9ab6f14fc8ceb1396c0
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-06-18 20:06:22 +0200

    Add AuthService for handling authentication requests

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private baseUrl = 'http://localhost:3000/api/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  register(user: User): Observable<any> {
+    return this.http.post(`${this.baseUrl}/register`, user);
+  }
+
+  login(user: User): Observable<any> {
+    return this.http.post(`${this.baseUrl}/login`, { username: user.username, password: user.password });
+  }
 }
