0dfc7e28e8fb528e5d674fcc5c0eefd02876504d
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-06-20 09:23:50 +0200

    Implement register component with form and styling

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { User } from '../../../models/user';
+import { AuthService } from '../../../core/services/auth.service';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  user: User = { username: '', password: '' };
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  onSubmit(): void {
+    this.authService.register(this.user).subscribe(
+      response => {
+        console.log('Registration successful', response);
+      },
+      error => {
+        console.error('Registration error', error);
+      }
+    );
+  }
 }
