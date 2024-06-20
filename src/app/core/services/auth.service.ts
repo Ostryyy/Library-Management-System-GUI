4564e96d3b1d102be4c85888366b2feaed6f2189
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-06-20 10:43:02 +0200

    added function returning user role

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -3,20 +3,37 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../../models/user';
 import { environment } from '../../../environments/environment';
+import { logout, getToken } from '../signals/auth.signal';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   private baseUrl = `${environment.apiUrl}/users`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   register(user: User): Observable<any> {
     return this.http.post(`${this.baseUrl}/register`, user);
   }
 
   login(user: User): Observable<any> {
-    return this.http.post(`${this.baseUrl}/login`, { username: user.username, password: user.password });
+    return this.http.post(`${this.baseUrl}/login`, {
+      username: user.username,
+      password: user.password,
+    });
+  }
+
+  logout(): void {
+    logout();
+  }
+
+  getUserRole(): string | null {
+    const token = getToken();
+    if (token) {
+      const payload = JSON.parse(atob(token.split('.')[1]));
+      return payload.role;
+    }
+    return null;
   }
 }
