62090b403158c07bd10fe189dc93c82a168e5f07
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-06-20 13:51:42 +0200

    added navigation and menu hamburger for smaller devices

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -4,18 +4,34 @@ import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../../services/auth.service';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MatToolbar, MatButtonModule],
+  imports: [CommonModule, MatToolbar, MatButtonModule, MatIcon, MatMenu, MatMenuTrigger, MatMenuItem, MatMenu],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  constructor(private router: Router, private authService: AuthService) {}
+
   isLoggedIn = isLoggedIn;
 
-  constructor(private router: Router) {}
+  isAdmin(): boolean {
+    const role = this.authService.getUserRole();
+    return role === 'admin';
+  }
+  
+  navigateToHome(): void {
+    this.router.navigate(['/home']);
+  }
+
+  navigateToReservations(): void {
+    this.router.navigate(['/reservations']);
+  }
 
   logout(): void {
     logout();
