cd915a5a6497325418a1b039f00f5624b4c12e80
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-06-20 13:52:04 +0200

    Added new routes home and reservations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { RegisterComponent } from './features/auth/register/register.component';
+import { HomeComponent } from './features/home/home.component';
+import { authGuard } from './core/guards/auth.guard';
+import { UserReservationsComponent } from './features/user-reservations/user-reservations.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+  {
+    path: 'reservations',
+    component: UserReservationsComponent,
+    canActivate: [authGuard],
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
